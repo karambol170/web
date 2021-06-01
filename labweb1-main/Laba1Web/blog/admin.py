1de855e56e8db7fe0addbc670039d31a9517402b
karambol170 <45936266+karambol170@users.noreply.github.com>
2021-06-01 21:31:46 +0300

    Delete admin.py

--- labweb1-main/Laba1Web/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models as M
-# Register your models here.
-
-admin.site.register(M.BlogUser)
-admin.site.register(M.BlogPost)
-admin.site.register(M.Comment)
-admin.site.register(M.Vote)
